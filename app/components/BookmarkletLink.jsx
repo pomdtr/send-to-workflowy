ddef0e421c04981bdcb60e44702e33c6776b63a0
cjlm <christian@cjlm.ca>
2021-11-09 20:32:17 +0000

    fix: Use vite import to get bookmarklet code rather than fetch

--- app/components/BookmarkletLink.jsx
+++ app/components/BookmarkletLink.jsx
@@ -1,28 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from '@chakra-ui/react';
 
+import bookmarkletRaw from '/bookmarklet.min.js?raw'
+
 export default function BookmarkletLink(props) {
   const { sessionId, parentId, children } = props;
-  const [code, setCode] = useState('');
   const [link, setLink] = useState('');
 
   useEffect(() => {
-    const getBookmarklet = async () => {
-      const response = await fetch('/bookmarklet.min.js');
-      setCode(await response.text());
-    };
-
-    getBookmarklet();
-  }, []);
-
-  useEffect(() => {
     setLink(
-      `javascript:${code
+      `javascript:${bookmarkletRaw
         .replace(`!SESSION_ID`, sessionId)
         .replace(`!PARENT_ID`, parentId)
         .replace(`!URL`, `${document.URL}send`)}`
     );
-  }, [code, sessionId, parentId]);
+  }, [sessionId, parentId]);
 
   return <Link href={link}>{children}</Link>;
 }
